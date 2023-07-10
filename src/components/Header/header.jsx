da97aded162ef1577280cc8562bfdb6506283311
VijiB98 <vijaybvijay1998@gmail.com>
2023-07-10 19:03:34 +0530

    push header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+/** @format */
+
+import React, { useState } from "react";
+import logo from "../../assets/icons/name.svg";
+import "./header.css";
+import Workspace from "../workspace_header/workspace_header";
 const Header = () => {
-  return <div></div>;
+  const [setState] = useState({
+    image: logo,
+  });
+  return (
+    <div className="header_main">
+      <img src={logo} alt="name" />
+    </div>
+  );
 };
 export default Header;
