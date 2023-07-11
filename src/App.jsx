392ccfcd45c9d0fa9502a89062f49245c3e7aed6
VijiB98 <vijaybvijay1998@gmail.com>
2023-07-11 10:51:58 +0530

    pull homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,56 @@
-<<<<<<< HEAD
-import { useState } from "react";
-<<<<<<< HEAD
+// /** @format */
 
-=======
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Login_Page from "./components/Login_page/login_page";
+ <<<<<<< HEAD
+ import { useState } from "react";
+ <<<<<<< HEAD
 
-function App() {
-  const [count, setCount] = useState(0);
+ =======
+ import reactLogo from "./assets/react.svg";
+ import viteLogo from "/vite.svg";
+ import "./App.css";
+ import Login_Page from "./components/Login_page/login_page";
 
-  return (
-    <>
+ function App() {
+   const [count, setCount] = useState(0);
+
+   return (
+     <>
       <div>
-        <Login_Page />
-      </div>
->>>>>>> bc8810b2481a491c3065cf00bc9564118dcf564c
-=======
-/** @format */
-
-import React, { useState } from "react";
+         <Login_Page />
+       </div>
+ >>>>>>> bc8810b2481a491c3065cf00bc9564118dcf564c
+ =======
+ /** @format */
+ import React, { useState } from "react";
 >>>>>>> da97aded162ef1577280cc8562bfdb6506283311
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+ import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header/header";
-<<<<<<< HEAD
-import Assigntasks from "./components/Assigntasks/assigntasks";
-import Timelinegraph from "./components/Timelinegraph/timelinegraph";
+ import Header from "./components/Header/header";
+ <<<<<<< HEAD
+ import Assigntasks from "./components/Assigntasks/assigntasks";
+ import Timelinegraph from "./components/Timelinegraph/timelinegraph";
 import Task from "./components/Assigntasks";
 
-function App() {
+ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+   <>
       <div>
         <Header />
         <Task />
       </div>
-=======
-import Workspace from "./components/workspace_header/workspace_header";
+ =======
+ import Workspace from "./components/workspace_header/workspace_header";
 
-function App() {
+ function App() {
   return (
-    <>
-      <Header />
->>>>>>> da97aded162ef1577280cc8562bfdb6506283311
-    </>
+     <>
+       <Header />
+ >>>>>>> da97aded162ef1577280cc8562bfdb6506283311
+     </>
   );
-}
+ }
 
-export default App;
+ export default App;
