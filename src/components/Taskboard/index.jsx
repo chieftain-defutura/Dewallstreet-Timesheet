d558fb064d520f08db6edeccf96c6c2697ab2c52
gd019946@gmail.com <gd019946@gmail.com>
2023-07-12 18:41:25 +0530

    push homepage

--- src/components/Taskboard/index.jsx
+++ src/components/Taskboard/index.jsx
@@ -3,11 +3,13 @@
 import React from "react";
 import Homeheader from "../Header/Homeheader";
 import Designpage from "./Designsystem/designsystem";
+import Assigntasks from "../Assigntasks/assigntasks";
 
 const Taskboard = () => {
   return (
     <div>
       <Homeheader />
+      <Assigntasks />
       <Designpage />
     </div>
   );
