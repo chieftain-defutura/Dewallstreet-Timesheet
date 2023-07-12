a25ae818cb0ad70dd898332c4415ad4e95ea526a
VijiB98 <vijaybvijay1998@gmail.com>
2023-07-12 11:30:57 +0530

    pull homepage

--- src/components/Taskboard/index.jsx
+++ src/components/Taskboard/index.jsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import Homeheader from "../Header/Homeheader";
+import Designpage from "./Designsystem/designsystem";
 
 const Taskboard = () => {
   return (
     <div>
       <Homeheader />
+      <Designpage />
     </div>
   );
 };
