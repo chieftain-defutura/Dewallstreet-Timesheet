71c90dcd54d921d5e1ef54c2131bc9e05a878372
Dhanvarshinikarthi <dhankarthi1708@gmail.com>
2023-07-14 18:55:20 +0530

    chore:pull logs page

--- src/components/Taskboard/index.jsx
+++ src/components/Taskboard/index.jsx
@@ -5,12 +5,13 @@ import Homeheader from "../Header/Homeheader";
 import Assigntasks from "../Assigntasks/assigntasks";
 import Designpage from "./Log/Logs";
 import Seeless from "../see more/seemore";
+import CommitPopup from "../commit/commit";
 
 const Taskboard = () => {
   return (
     <div>
       <Designpage />
-      {/* <Seeless /> */}
+      <CommitPopup />
     </div>
   );
 };
--- src/components/commit/commit.jsx
+++ src/components/commit/commit.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./commit.css";
+import cancel from "../../assets/icons/cancel.svg";
+import commitimage from "../../assets/icons/commitimage.svg";
+import Button from "../Button";
 const CommitPopup = () => {
   return (
     <div className="commit_main">
       <div className="commit_header">
         <h5>Commit.</h5>
+        <img src={cancel} alt="cancel" />
       </div>
+      <div className="commit_emptyline"></div>
+      <img src={commitimage} alt="" />
+      <Button variant="primary" size="large">Confirm</Button>
     </div>
   );
 };
