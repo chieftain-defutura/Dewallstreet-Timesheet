a5ad4863c2c19264e988de23e26447a226805966
Dhanvarshinikarthi <dhankarthi1708@gmail.com>
2023-07-14 16:22:27 +0530

    chore:pull seemore button changes

---
+++ src/components/commit/commit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./commit.css";
+const CommitPopup = () => {
+  return (
+    <div className="commit_main">
+      <div className="commit_header">
+        <h5>Commit.</h5>
+      </div>
+    </div>
+  );
+};
+export default CommitPopup;
