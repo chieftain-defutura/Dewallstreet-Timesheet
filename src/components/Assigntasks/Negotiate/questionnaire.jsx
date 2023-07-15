00ea31d04aa5a479512ea20debf8630e4d4c15bb
gd019946@gmail.com <gd019946@gmail.com>
2023-07-15 10:44:53 +0530

    chore: pull main

--- src/components/Assigntasks/Negotiate/questionnaire.jsx
+++ src/components/Assigntasks/Negotiate/questionnaire.jsx
@@ -1,30 +1,118 @@
 import React from "react";
 import "./questionnaire.css";
 import plus from "../../../assets/icons/plus.svg";
+import linkicon from "../../../assets/icons/link.svg";
+import documenticon from "../../../assets/icons/document.svg";
 import Button from "../../Button";
+import { render } from "react-dom";
 const Questionnaire = () => {
-  return (
-    <div className="questionnaire_main">
-      <h3>Negotiate.</h3>
-      <div className="all_questionnaires">
-        <div className="questionnaire_head">
-          <div className="question">
-            <a href="#">Questionnaire</a>
-            <div className="horizontal_line_questionnaire"></div>
-          </div>
-
-          <div className="meet">
-            <a href="#">Meeting</a>
-            <div className="horizontal_line_meeting"></div>
-          </div>
+  const Questionnairetwo = [
+    {
+      id: 1,
+      heading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+      icon1: linkicon,
+      icon1para:
+        "Ut enim ad minim veniam, quis nostrod exercitation ullamco laboris.",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      icon2: documenticon,
+      icon2para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 2,
+      heading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+      icon1: linkicon,
+      icon1para:
+        "Ut enim ad minim veniam, quis nostrod exercitation ullamco laboris.",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      icon2: documenticon,
+      icon2para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 3,
+      heading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+      icon1: linkicon,
+      icon1para:
+        "Ut enim ad minim veniam, quis nostrod exercitation ullamco laboris.",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      icon2: documenticon,
+      icon2para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
+  const renderQuestionnairetwo = Questionnairetwo.map((questtwolist) => {
+    return (
+      <div className="individual_questionnaire">
+        <div className="questionnaire_number">
+          <h5>{questtwolist.id}</h5>
+          <h5>{questtwolist.heading}</h5>
+        </div>
+        <div className="link_para">
+          <img src={questtwolist.icon1} alt="icon" />
+          <h5> {questtwolist.icon1para} </h5>
+        </div>
+        <p> {questtwolist.paragraph} </p>
+        <div className="link_para" id="link_para_another">
+          <img src={questtwolist.icon1} alt="icon" />
+          <h5> {questtwolist.icon1para} </h5>
+        </div>
+        <div className="file_para">
+          <img src={questtwolist.icon2} alt="icon" />
+          <h5>{questtwolist.icon2para}</h5>
         </div>
       </div>
-      <div className="qnbtn">
-        <Button variant="secondary" size="medium" image={plus}>
-          Add questionnaire
-        </Button>
+    );
+  });
+  return (
+    <React.Fragment>
+      <div className="questionnaire_main">
+        <div className="all_questionnaires">
+          <h3>Negotiate.</h3>
+          <div className="questionnaire_head">
+            <div className="question">
+              <a href="#">Questionnaire</a>
+              <div className="horizontal_line_questionnaire"></div>
+            </div>
+
+            <div className="meet">
+              <a href="#">Meeting</a>
+              <div className="horizontal_line_meeting"></div>
+            </div>
+          </div>
+          <div className="question_button">
+            <Button variant="secondary" size="medium" image={plus}>
+              Add questionnaire
+            </Button>
+          </div>
+          <div className="questionnaire_one_all">
+            <div className="questionnaire_one">
+              <h4>Questionnaire 1</h4>
+              <div className="wait_button">
+                <Button variant="secondary" size="small">
+                  Waiting for answer
+                </Button>
+              </div>
+            </div>
+            <div className="ask_date">
+              <h4>ASKED DATE</h4>
+              <h4 id="date">08 June 2023.</h4>
+            </div>
+          </div>
+        </div>
+        {/* <React.Fragment> */}
+        <div className="questionnaire_two_all">
+          <div className="questionnaire_two_head">
+            <h4>Questionnaire 2</h4>
+            <Button variant="secondary" size="small">
+              Answered
+            </Button>
+          </div>
+          <div className="questionnaire_two">{renderQuestionnairetwo}</div>
+        </div>
+        {/* </React.Fragment> */}
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 export default Questionnaire;
--- src/components/Taskboard/index.jsx
+++ src/components/Taskboard/index.jsx
@@ -1,17 +1,24 @@
 /** @format */
 
 import React from "react";
+import "./taskboard.css";
 import Homeheader from "../Header/Homeheader";
 import Assigntasks from "../Assigntasks/assigntasks";
 import Designpage from "./Log/Logs";
 import Seeless from "../see more/seemore";
 import CommitPopup from "../commit/commit";
+import Questionnaire from "../Assigntasks/Negotiate/questionnaire";
 
 const Taskboard = () => {
   return (
-    <div className="design_questionnaire">
-      <Designpage />
-      <CommitPopup />
+    <div>
+      <div className="design_questionnaire">
+        <Designpage />
+        <Questionnaire />
+      </div>
+      <>
+        <CommitPopup />
+      </>
     </div>
   );
 };
