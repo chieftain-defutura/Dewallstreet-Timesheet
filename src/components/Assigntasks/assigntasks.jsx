78e92a9c21c27df7121e69cc7503e74dc56f586c
gd019946@gmail.com <gd019946@gmail.com>
2023-07-19 13:05:25 +0530

    chore: push page

--- src/components/Assigntasks/assigntasks.jsx
+++ src/components/Assigntasks/assigntasks.jsx
@@ -15,9 +15,9 @@ const Assigntasks = ({ props, image }) => {
   return (
     <div className="assign_tasks_main">
       <div className="clock_work">
-        <li className={splitLocation[1] === "clockwork" ? "active" : ""}>
-          <Link to="/home">
-            {/* {pathname === "/home" ? (
+        {/* <li className={splitLocation[1] === "clockwork" ? "active" : ""}> */}
+        <Link to="/home">
+          {/* {pathname === "/home" ? (
               <img src={briefcasetimer} alt="icon" />
             ) : (
               <img src={briefcasetimerhide} alt="icon" />
@@ -37,59 +37,59 @@ const Assigntasks = ({ props, image }) => {
                 Works
               </h4>
             )} */}
-            {pathname === "/home" ? (
-              <>
-                <div onClick={() => setToggle(toggle)}>
-                  <img src={briefcasetimer} alt="icon" />
-                  <h4 style={{ color: "white" }}>
-                    Clock
-                    <br />
-                    Works
-                  </h4>
-                </div>
-              </>
-            ) : (
-              <>
-                <div onClick={() => setToggle(toggle)}>
-                  <img src={briefcasetimerhide} alt="icon" />
-                </div>
-                <div>
-                  <h4 style={{ color: "grey" }}>
-                    Clock
-                    <br />
-                    Works
-                  </h4>
-                </div>
-              </>
-            )}
-          </Link>
-        </li>
+          {pathname === "/home" ? (
+            <>
+              <div onClick={() => setToggle(toggle)}>
+                <img src={briefcasetimer} alt="icon" />
+                <h4 style={{ color: "white" }}>
+                  Clock
+                  <br />
+                  Works
+                </h4>
+              </div>
+            </>
+          ) : (
+            <>
+              <div onClick={() => setToggle(toggle)}>
+                <img src={briefcasetimerhide} alt="icon" />
+              </div>
+              <div>
+                <h4 style={{ color: "grey" }}>
+                  Clock
+                  <br />
+                  Works
+                </h4>
+              </div>
+            </>
+          )}
+        </Link>
+        {/* </li> */}
       </div>
       <div className="horizontal_line"></div>
       <div className="task_board">
-        <li className={splitLocation[1] === "taskboard" ? "active" : ""}>
-          <Link to="/taskpage">
-            {pathname === "/taskpage" ? (
-              <img src={tasksheethide} alt="icon" />
-            ) : (
-              <img src={tasksheet} alt="icon" />
-            )}
+        {/* <li className={splitLocation[1] === "taskboard" ? "active" : ""}> */}
+        <Link to="/taskpage">
+          {pathname === "/taskpage" ? (
+            <img src={tasksheethide} alt="icon" />
+          ) : (
+            <img src={tasksheet} alt="icon" />
+          )}
 
-            {pathname === "/taskpage" ? (
-              <h4 style={{ color: "white" }}>
-                Task
-                <br />
-                Board
-              </h4>
-            ) : (
-              <h4 style={{ color: "grey" }}>
-                Task
-                <br />
-                Board
-              </h4>
-            )}
-          </Link>
-        </li>
+          {pathname === "/taskpage" ? (
+            <h4 style={{ color: "white" }}>
+              Task
+              <br />
+              Board
+            </h4>
+          ) : (
+            <h4 style={{ color: "grey" }}>
+              Task
+              <br />
+              Board
+            </h4>
+          )}
+        </Link>
+        {/* </li> */}
       </div>
     </div>
   );
