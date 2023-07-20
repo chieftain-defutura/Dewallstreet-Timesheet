3db315e1ea169d342087bb6b680951252e09d469
gd019946@gmail.com <gd019946@gmail.com>
2023-07-20 16:03:34 +0530

    chore: push audits

---
+++ src/components/Archivedtasks/Archivedtasks.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./Archivedtasks.css";
+import downarrow from "../../assets/icons/archivedown.svg";
+import uparrow from "../../assets/icons/archiveup.svg";
+import { Archivetasklist } from "./Archivetask";
+
+import Button from "../Button";
+import Archivedtasksseemore from "../Archivedtasksseemore/Archivedtasksseemore";
+import AuditDesignsystem from "../Taskboard/Audits/Audits";
+const Archivedesignsystem = ({}) => {
+  console.log();
+  const RenderArchivedesign = Archivetasklist.map((Designtype, i) => {
+    return <Archivecontent key={i} data={Designtype} />;
+  });
+  return (
+    <React.Fragment>
+      <div className="archive_render">{RenderArchivedesign}</div>
+    </React.Fragment>
+  );
+};
+export default Archivedesignsystem;
+const Archivecontent = ({ data }) => {
+  const [seemore, setSeemore] = useState(false);
+ 
+  console.log("data", data);
+  if (!data) return;
+  return (
+    <div className="task_system">
+      <div className="task_title">
+        <h1>{data.Title}</h1>
+        <p>{data.Para}</p>
+      </div>
+
+      {!seemore && (
+        <>
+          <div className="passed">
+            <Button variant="secondary" size="small">
+              Passed
+            </Button>
+          </div>
+          <div className="arrows">
+            <button onClick={() => setSeemore(true)}>
+              {data.More}
+              <img src={downarrow} alt="icon" />
+            </button>
+          </div>
+        </>
+      )}
+      {/* <Button variant="primary" size="small">
+        Passed
+      </Button>
+      <div className="arrows">
+        <button>
+          {data.more}
+          <img src={downarrow} alt="icon" />
+        </button>
+      </div> */}
+      {seemore && (
+        <Archivedtasksseemore Designtype={data} setSeemore={setSeemore} />
+      )}
+      {/* {!archivetasks && (
+        <>
+          <div className="arrows">
+            <button onClick={() => setSeemore(true)}>
+              {data.More}
+              {<img src={downarrow} alt="icon" />}
+            </button>
+          </div>
+        </>
+      )}
+      {archivetasks && (
+        <Archivedesignsystem
+          Designtype={data}
+          setArchivetasks={setArchivetasks}
+        />
+      )} */}
+    </div>
+  );
+};
