82a97dc78a09ffdfd635c22317ca218d86f08d29
Dhanvarshinikarthi <dhankarthi1708@gmail.com>
2023-07-26 18:06:12 +0530

    chore:push working hours functioning

--- src/components/Assigntasks/WorkingHours/index.jsx
+++ src/components/Assigntasks/WorkingHours/index.jsx
@@ -1,17 +1,25 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import "./WorkingHours.css";
 import hoursprograss from "../../../assets/icons/hoursprograss.svg";
 import minutesprograss from "../../../assets/icons/minutesprograss.svg";
 import secondsprograss from "../../../assets/icons/secondsprograss.svg";
 import suitcase from "../../../assets/icons/suitcase.svg";
 import gracetime from "../../../assets/icons/gracetime.svg";
+import gpslocation from "../../../assets/icons/gpslocation.svg";
+import location from "../../../assets/icons/location.svg";
+import turnon from "../../../assets/icons/turnonimage.svg";
+import firstleveldark from "../../../assets/icons/firstleveldark.svg";
 import firstlevel from "../../../assets/icons/firstlevel.svg";
 import secondlevel from "../../../assets/icons/secondlevel.svg";
-import thirdlevel from "../../../assets/icons/thirdlevel.svg";
-import fourthlevel from "../../../assets/icons/fourthlevel.svg";
 import secondleveldark from "../../../assets/icons/lunchimage.svg";
+import thirdlevel from "../../../assets/icons/thirdlevel.svg";
+import thirdleveldark from "../../../assets/icons/thirdleveldark.svg";
+import fourthlevel from "../../../assets/icons/fourthlevel.svg";
+import fourthleveldark from "../../../assets/icons/fourthlevel.svg";
+import breakimage from "../../../assets/icons/breakimage.svg";
+import Checkout from "../../../assets/icons/checkout.svg";
 import cancel from "../../../assets/icons/cancel.svg";
 import Checkin from "../../../assets/icons/checkin.svg";
 import usezap from "../../../assets/icons/usezap.svg";
@@ -21,12 +29,19 @@ import warning from "../../../assets/icons/warning.svg";
 import Button from "../../Button";
 import Toasts from "../../Toasts";
 import LayoutModal from "../../Modal/Modal";
-
 const WorkingHours = () => {
   const [open, setOpen] = useState(false);
-  const [checkin, setCheckIn] = useState(false);
+  // const [checkin, setCheckIn] = useState(false);
+  // const [checkout, setCheckout] = useState(false);
   const [use, setUse] = useState(false);
-  const [lunch, setLunch] = useState(false);
+  const [gps, setGps] = useState(false);
+  const [steps, setSteps] = useState(0);
+  const [active, setActive] = useState();
+  const [lunchtime, setLunchtime] = useState();
+  const [breaktime, setBreaktime] = useState();
+  const [exit, setExit] = useState();
+  console.log(steps);
+
   return (
     <>
       <div className="workingcheckin_page">
@@ -34,7 +49,6 @@ const WorkingHours = () => {
           <div className="time_checkinhours">
             <div className="check_workinghours">
               <h4>Working hours.</h4>
-
               <div className="clock_timing">
                 <div className="hours_prograssbar">
                   <img src={hoursprograss} alt="hours" />
@@ -51,7 +65,21 @@ const WorkingHours = () => {
               </div>
             </div>
 
-            {checkin ? (
+            {steps === 0 && (
+              <div className="checkin">
+                <div className="checkin_seconds">
+                  <img src={suitcase} alt="suitcase" />
+                  <div className="check">
+                    <h5>Morning check in starts in</h5>
+                    <h4>05m 00s</h4>
+                  </div>
+                </div>
+                <Button variant="primary" size="regular" image={Checkin}>
+                  Check in
+                </Button>
+              </div>
+            )}
+            {steps === 1 && (
               <div className="checkin">
                 <div className="checkin_seconds">
                   <img src={usetimer} alt="suitcase" />
@@ -69,24 +97,25 @@ const WorkingHours = () => {
                   Use
                 </Button>
               </div>
-            ) : (
-              <div className="checkin">
-                <div className="checkin_seconds">
-                  <img src={suitcase} alt="suitcase" />
-                  <div className="check">
-                    <h5>Morning check in starts in</h5>
-                    <h4>05m 00s</h4>
-                  </div>
-                </div>
-                <Button variant="primary" size="regular" image={Checkin}>
-                  Check in
-                </Button>
-              </div>
             )}
-            {!checkin && lunch && (
+            {steps === 2 && (
               <div className="checkin">
                 <div className="checkin_seconds">
                   <img src={secondleveldark} alt="suitcase" />
+                  <div className="check">
+                    <h5>Lunch break starts in</h5>
+                    <h4>05m 00s</h4>
+                  </div>
+                </div>
+                <Button variant="checkout" size="regular" image={Checkout}>
+                  Check out
+                </Button>
+              </div>
+            )}
+            {steps === 3 && (
+              <div className="checkin">
+                <div className="checkin_seconds">
+                  <img src={breakimage} alt="breakimage" />
                   <div className="check">
                     <h5>Lunch break starts in</h5>
                     <h4>05m 00s</h4>
@@ -97,46 +126,143 @@ const WorkingHours = () => {
                 </Button>
               </div>
             )}
+            {gps && (
+              <LayoutModal>
+                <div className="gps_location">
+                  <div className="gpslocation_header">
+                    <h5>GPS location</h5>
+                    <button onClick={() => setGps(false)}>
+                      <img src={cancel} alt="cancel" />
+                    </button>{" "}
+                  </div>
+                  <div className="gps_emptyline"></div>
+                  <h3>Your GPS location was not found.</h3>
+                  <img src={gpslocation} alt="gpslocation" />
+                  <h4>
+                    Please make sure that you are in one of the locations.
+                  </h4>
+                  <div className="gpslocation_para">
+                    <div className="location_para">
+                      <img src={location} alt="location" />
+                      <p>
+                        Dewallstreet, No.1, Mahalakshmi Nagar, East Tambaram.
+                      </p>
+                    </div>
+                    <div className="location_para">
+                      <img src={location} alt="location" />
+                      <p>
+                        Dehustle institute,No.1, Mahalakshmi Nagar, East
+                        <p>Tambaram.</p>
+                      </p>
+                    </div>
+                    <div className="location_para">
+                      <img src={location} alt="location" />
+                      <p>Deaccel, No.1, Mahalakshmi Nagar, East Tambaram.</p>
+                    </div>
+                  </div>
+                  <Button
+                    variant="primary"
+                    size="large"
+                    image={turnon}
+                    handleClick={() => setGps(false)}
+                  >
+                    Turn on GPS
+                  </Button>
+                </div>
+              </LayoutModal>
+            )}
           </div>
+
           <div className="centeremptyline"></div>
           <div className="level_progress">
             <div className="checkin_levels">
               <div className="firstlevel_timing">
-                <div
-                  className="first_level"
-                  onClick={() => setCheckIn((m) => !m)}
-                >
-                  <img src={firstlevel} alt="first" />
-                </div>
+                {active === "checkin" ? (
+                  <div
+                    className="first_level"
+                    onClick={() => {
+                      setSteps(1);
+                      setLunchtime(false);
+                      setActive(true);
+                      setBreaktime(false);
+                    }}
+                  >
+                    <img src={firstlevel} alt="first" />
+                  </div>
+                ) : (
+                  <div
+                    className="first_level"
+                    onClick={() => {
+                      setSteps(1);
+                      setLunchtime("lunch");
+                      setActive("checkin");
+                    }}
+                  >
+                    <img src={firstleveldark} alt="first" />
+                  </div>
+                )}
+
                 <p>10:00AM</p>
               </div>
-              {lunch ? (
-                <div className="secondlevel_timing">
+
+              <div className="secondlevel_timing">
+                {lunchtime === "lunch" ? (
                   <div
                     className="second_level"
-                    onClick={() => setCheckIn(false)}
+                    onClick={() => {
+                      setSteps(2);
+                      setActive(false);
+                      setLunchtime(true);
+                      setBreaktime(false);
+                    }}
                   >
                     <img src={secondleveldark} alt="second" />
                   </div>
-                  <p>01:00PM-02:00PM</p>
-                </div>
-              ) : (
-                <div className="secondlevel_timing">
-                  <div className="second_level" onClick={() => setLunch(true)}>
+                ) : (
+                  <div
+                    className="second_level"
+                    onClick={() => {
+                      setSteps(2);
+                      setLunchtime("lunch");
+                      setActive("checkin");
+                    }}
+                  >
                     <img src={secondlevel} alt="second" />
                   </div>
-                  <p>01:00PM-02:00PM</p>
-                </div>
-              )}
+                )}
+                <p>01:00PM-02:00PM</p>
+              </div>
 
               <div className="thirdlevel_timing">
-                <div className="third_level">
-                  <img src={thirdlevel} alt="third" />
-                </div>
+                {breaktime === "break" ? (
+                  <div
+                    className="third_level"
+                    onClick={() => {
+                      setSteps(3);
+                      setBreaktime(true);
+                      setLunchtime(false);
+                      setActive(false);
+                    }}
+                  >
+                    <img src={thirdleveldark} alt="third" />
+                  </div>
+                ) : (
+                  <div
+                    className="third_level"
+                    onClick={() => {
+                      setSteps(3);
+                      setBreaktime("break");
+                      setLunchtime("lunch");
+                    }}
+                  >
+                    <img src={thirdlevel} alt="third" />
+                  </div>
+                )}
+
                 <p>05:00PM-05:30PM</p>
               </div>
               <div className="fourthlevel_timing">
-                <div className="fourth_level">
+                <div className="fourth_level" onClick={() => setGps(true)}>
                   <img src={fourthlevel} alt="fourth" />
                 </div>
                 <p>06:30PM</p>
