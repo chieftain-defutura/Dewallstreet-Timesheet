ca05035f2213cb777851feb7b6f2151da7ea6f14
gd019946@gmail.com <gd019946@gmail.com>
2023-08-23 11:10:36 +0530

    chore: push corrections

--- src/components/Assigntasks/Negotiate/index.jsx
+++ src/components/Assigntasks/Negotiate/index.jsx
@@ -8,80 +8,81 @@ const Negotiate = () => {
   const [negotiateopen, setNegotiateOpen] = useState(false);
   return (
     <>
-      <div className="questionnaire_main">
-        <div className="negotiate_header">
-          <h3>Negotiate.</h3>
-          {negotiateopen && (
-            <img
-              src={cancel}
-              alt="cancel"
-              width="20px"
-              onClick={() => setNegotiateOpen(true)}
-            />
-          )}
+      {!negotiateopen && (
+        <div className="questionnaire_main">
+          <div
+            className="negotiate_header"
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <h3>Negotiate.</h3>
+
+            <button onClick={() => setNegotiateOpen(true)}>
+              <img src={cancel} alt="cancel" width="20px" />
+            </button>
+          </div>
+          <div className="questionnaire_head">
+            {toggle === "Questionnaire" ? (
+              <div
+                className="negotiate_link"
+                style={{
+                  borderBottom: "1px solid #fff",
+                  display: "grid",
+                  textAlign: "center",
+                }}
+                onClick={() => setToggle("Questionnaire")}
+              >
+                <a href="#" style={{ color: "#fff" }}>
+                  Questionnaire
+                </a>
+              </div>
+            ) : (
+              <div
+                style={{
+                  display: "grid",
+                  textAlign: "center",
+                  borderBottom: "1px solid grey",
+                }}
+                className="negotiate_link"
+                onClick={() => setToggle("Questionnaire")}
+              >
+                <a href="#" style={{ color: "grey" }}>
+                  Questionnaire
+                </a>
+              </div>
+            )}
+            {toggle === "meeting" ? (
+              <div
+                className="negotiate_link"
+                style={{
+                  borderBottom: "1px solid #fff",
+                  textAlign: "center",
+                  display: "grid",
+                }}
+                onClick={() => setToggle("meeting")}
+              >
+                <a href="#" style={{ color: "#fff" }}>
+                  Meeting
+                </a>
+              </div>
+            ) : (
+              <div
+                style={{
+                  display: "grid",
+                  textAlign: "center",
+                  borderBottom: "1px solid grey",
+                }}
+                className="negotiate_link"
+                onClick={() => setToggle("meeting")}
+              >
+                <a href="#" style={{ color: "grey" }}>
+                  Meeting
+                </a>
+              </div>
+            )}
+          </div>
+          {toggle === "meeting" ? <Meeting /> : <QuestionContent />}
         </div>
-        <div className="questionnaire_head">
-          {toggle === "Questionnaire" ? (
-            <div
-              className="negotiate_link"
-              style={{
-                borderBottom: "1px solid #fff",
-                display: "grid",
-                textAlign: "center",
-              }}
-              onClick={() => setToggle("Questionnaire")}
-            >
-              <a href="#" style={{ color: "#fff" }}>
-                Questionnaire
-              </a>
-            </div>
-          ) : (
-            <div
-              style={{
-                display: "grid",
-                textAlign: "center",
-                borderBottom: "1px solid grey",
-              }}
-              className="negotiate_link"
-              onClick={() => setToggle("Questionnaire")}
-            >
-              <a href="#" style={{ color: "grey" }}>
-                Questionnaire
-              </a>
-            </div>
-          )}
-          {toggle === "meeting" ? (
-            <div
-              className="negotiate_link"
-              style={{
-                borderBottom: "1px solid #fff",
-                textAlign: "center",
-                display: "grid",
-              }}
-              onClick={() => setToggle("meeting")}
-            >
-              <a href="#" style={{ color: "#fff" }}>
-                Meeting
-              </a>
-            </div>
-          ) : (
-            <div
-              style={{
-                display: "grid",
-                textAlign: "center",
-                borderBottom: "1px solid grey",
-              }}
-              className="negotiate_link"
-              onClick={() => setToggle("meeting")}
-            >
-              <a href="#" style={{ color: "grey" }}>
-                Meeting
-              </a>
-            </div>
-          )}
-        </div>
-        {toggle === "meeting" ? <Meeting /> : <QuestionContent />}
-      </div>
+      )}
     </>
   );
 };
